7321d4256e93b8cc0b12407c644aa905eb4cfd37
Caleb <xiaoguang1988@gmail.com>
2016-09-25 23:59:25 -0400

    tesing BIC4

--- R/ISKmeans.R
+++ R/ISKmeans.R
@@ -134,9 +134,9 @@ function(d, K=NULL, gamma=NULL, alpha=0.5, group=NULL, nstart=20, wsPre=NULL ,sp
 	  BIC3 <- (n - 1) * sum(ifelse(ws!=0,1,0) * wcss$r) - log(n) * sum(ws!=0)
 
 	  BIC <-  - sum(ws * (1 - wcss$r)) - log(n*2*pi) * sum(ws)
-	  BIC1 <-  - sum(sum(ifelse(ws!=0,1,0) * (1 - wcss$r)) - log(n*2*pi) * sum(ws)
+	  BIC1 <-  - sum(ifelse(ws!=0,1,0) * (1 - wcss$r)) - log(n*2*pi) * sum(ws)
 	  BIC2 <-  - sum(ws * (1 - wcss$r)) - log(n*2*pi) * sum(ws!=0)
-	  BIC3 <-  - sum(sum(ifelse(ws!=0,1,0) * (1 - wcss$r)) - log(n*2*pi) * sum(ws!=0)
+	  BIC3 <-  - sum(ifelse(ws!=0,1,0) * (1 - wcss$r)) - log(n*2*pi) * sum(ws!=0)
 
 	  out[[i]] <- list(ws=ws, Cs=Cs, objective=ADMMobject$objective, BIC=BIC, BIC1=BIC1, BIC2=BIC2, BIC3=BIC3 , gamma=agamma,alpha=alpha)
 
