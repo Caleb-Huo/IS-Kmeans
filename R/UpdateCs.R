31843ec31fe372e56bae83941acc797c2095aef0
Caleb <xiaoguang1988@gmail.com>
2016-09-28 19:36:01 -0400

    debug20160928_9

--- R/UpdateCs.R
+++ R/UpdateCs.R
@@ -1,6 +1,6 @@
 UpdateCs <- function(x, K, ws, Cs){
   x <- x[,ws!=0]
-  if(sum(ws!=0)==1){
+  if(sum(ws!=0)==1){ 
 	  z <- x * sqrt(ws[ws!=0])
   } else {
 	  z <- sweep(x, 2, sqrt(ws[ws!=0]), "*")  	
