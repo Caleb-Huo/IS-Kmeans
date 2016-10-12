888a86283a41565cead8152203eb769a9879d64f
Caleb <xiaoguang1988@gmail.com>
2016-10-11 20:50:49 -0400

    adaptive group penalty 7

--- src/ISKmeans.cpp
+++ src/ISKmeans.cpp
@@ -475,8 +475,8 @@ void ADMM_updatew(double *x, double *y, double *z, double *r, double *objective,
 	
 	// update z by x
 	updateZbyX(x, z, groupLevel, genePos, coef, J, G);
-	updateX(x, y, z, groupLevel,genePos,coef, J, G, L, rho);
-	updateZ(x, y, z, r, groupLevel, genePos,coef, J, G, L, rho);	
+	//updateX(x, y, z, groupLevel,genePos,coef, J, G, L, rho);
+	//updateZ(x, y, z, r, groupLevel, genePos,coef, J, G, L, rho);	
     //free(z_old);
 	
 }
