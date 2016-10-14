7d08311abcbea959bbc630a09337f75a1646bc7e
Caleb <xiaoguang1988@gmail.com>
2016-10-14 16:47:45 -0400

    enhance ADMM convergence 20161014 12

--- R/updateISKmeans.R
+++ R/updateISKmeans.R
@@ -7,17 +7,26 @@ updateISKmeans <- function(d, K, groupInfo, Css, wss, silent=silent, maxiter=max
 cat('initilizaing results using alpha = 1, step 3\n')
 	
   while((sum(abs(wss-wss.old))/sum(abs(wss.old)))>1e-4 && niter<maxiter){
+  	cat('initilizaing results using alpha = 1, step 4\n')
     if(!silent) cat('Iteration',niter, ':\n', fill=FALSE)
+		cat('initilizaing results using alpha = 1, step 4\n')
     niter <- niter+1
+	cat('initilizaing results using alpha = 1, step 5\n')
     wss.old <- wss
+	cat('initilizaing results using alpha = 1, step 6\n')
 	if(sum(wss!=0)<1){
+		cat('initilizaing results using alpha = 1, step 7\n')
 		nonTrivialFlag<-0
+		cat('initilizaing results using alpha = 1, step 8\n')
 		wssPre <- wss
+		cat('initilizaing results using alpha = 1, step 9\n')
 		objective <- 0
+		cat('initilizaing results using alpha = 1, step 10\n')
 		obj0 <- 0
+		cat('initilizaing results using alpha = 1, step 11\n')
 		break
 	}
-	cat('initilizaing results using alpha = 1, step 4\n')
+	cat('initilizaing results using alpha = 1, step 12\n')
 	
 	if(!silent) cat('Updating CS...\n', fill=FALSE)
     if(niter>1) Css <- UpdateCss(d, K, wss, Css) # if niter=1, no need to update!!
