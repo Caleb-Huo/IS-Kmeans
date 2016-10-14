15a9e0561eedc4107fa0d0b91f7ac50177156704
Caleb <xiaoguang1988@gmail.com>
2016-10-14 16:50:05 -0400

    enhance ADMM convergence 20161014 14

--- R/updateISKmeans.R
+++ R/updateISKmeans.R
@@ -1,62 +1,50 @@
-updateISKmeans <- function(d, K, groupInfo, Css, wss, silent=silent, maxiter=maxiter){
+updateISKmeans <- function(d, K, groupInfo, Cs, ws, silent=TRUE, maxiter=20){
   J <- ncol(d)
-  wss.old <- rnorm(J)
+  ws.old <- rnorm(J)
   nonTrivialFlag = 1
   niter <- 0
   currentY <- NULL
-cat('initilizaing results using alpha = 1, step 3\n')
 	
-  while((sum(abs(wss-wss.old))/sum(abs(wss.old)))>1e-4 && niter<maxiter){
-  	cat('initilizaing results using alpha = 1, step 4\n')
+  while((sum(abs(ws-ws.old))/sum(abs(ws.old)))>1e-4 && niter<maxiter){
     if(!silent) cat('Iteration',niter, ':\n', fill=FALSE)
-		cat('initilizaing results using alpha = 1, step 4\n')
     niter <- niter+1
-	cat('initilizaing results using alpha = 1, step 5\n')
-    wss.old <- wss
-	cat('initilizaing results using alpha = 1, step 6\n')
-	if(sum(wss!=0)<1){
-		cat('initilizaing results using alpha = 1, step 7\n')
+    ws.old <- ws
+	if(sum(ws!=0)<1){
 		nonTrivialFlag<-0
-		cat('initilizaing results using alpha = 1, step 8\n')
-		wssPre <- wss
-		cat('initilizaing results using alpha = 1, step 9\n')
+		wsPre <- ws
 		objective <- 0
-		cat('initilizaing results using alpha = 1, step 10\n')
 		obj0 <- 0
-		cat('initilizaing results using alpha = 1, step 11\n')
 		break
 	}
-	cat('initilizaing results using alpha = 1, step 12\n')
-	
 	if(!silent) cat('Updating CS...\n', fill=FALSE)
-    if(niter>1) Css <- UpdateCss(d, K, wss, Css) # if niter=1, no need to update!!
+    if(niter>1) Cs <- UpdateCs(d, K, ws, Cs) # if niter=1, no need to update!!
  	if(!silent) cat('Updating WS...\n', fill=FALSE)
 	if(is.null(groupInfo)){
 		nonTrivialFlag<-0			
-		wcss=GetWCSS(d, Css)
-		wss <- wcss$r/sqrt(sum(wcss$r^2))
-		wssPre <- wss
-		objective <- - sum(wss * wcss$r)
-		obj0 <-  - sum(wss * wcss$r)			
+		wcss=GetWCSS(d, Cs)
+		ws <- wcss$r/sqrt(sum(wcss$r^2))
+		wsPre <- ws
+		objective <- - sum(ws * wcss$r)
+		obj0 <-  - sum(ws * wcss$r)			
 		print(objective)
 	} else {
-		ADMMobject <- UpdateWsADMM(d, Css, wss, currentY=currentY, groupInfo)
-		wss <- ADMMobject$z
+		ADMMobject <- UpdateWsADMM(d, Cs, ws, currentY=currentY, groupInfo)
+		ws <- ADMMobject$z
 		currentY <- ADMMobject$currentY	
 		print(ADMMobject$objective)
 				
 	}
   }
   if(nonTrivialFlag){
-	  ##wss[wss<sum(wss)/ncol(d)] <- 0
-	  Css <- UpdateCss(d, K, wss, Css)
-	  wcss=GetWCSS(d, Css)
-	  wssPre <- wss
+	  ##ws[ws<sum(ws)/ncol(d)] <- 0
+	  Cs <- UpdateCs(d, K, ws, Cs)
+	  wcss=GetWCSS(d, Cs)
+	  wsPre <- ws
 	  objective = ADMMobject$objective
-	  obj0 <-  - sum(wss * wcss$r)
+	  obj0 <-  - sum(ws * wcss$r)
 	  ## original implementation
-	  ## BIC <- (n - 1) * sum(wss * wcss$r) - log(n) * sum(wss)	  	
+	  ## BIC <- (n - 1) * sum(ws * wcss$r) - log(n) * sum(ws)	  	
   }
-  res <- list(wss=wss, Css=Css, obj0 = obj0, objective=objective, gamma=groupInfo$gamma,alpha=groupInfo$alpha)	  
+  res <- list(ws=ws, Cs=Cs, obj0 = obj0, objective=objective, gamma=groupInfo$gamma,alpha=groupInfo$alpha)	  
   return(res)
 }
